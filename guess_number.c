b9f929b32dbf1e0c9348617a266edc287feacfb0
uwasepauline7 <puwase@andrew.cmu.edu>
2024-09-08 19:47:02 +0200

    creating the logic to implement the game

--- guess_number.c
+++ guess_number.c
@@ -9,15 +9,16 @@ int main() {
     int guess;
     int attempts = 0;
 
-    printf("Think of a number between 1 and %d, and I'll try to guess it!\n", GUESS_RANGE);
+    printf("Think a number between 1 and %d, and I'll try to guess it!\n", GUESS_RANGE);
 
-    // Continue guessing while the range is valid (low <= high)
-    while (lower_bound <= higher_range) {
+    // Continue guessing using while loop to validate the range
+    while (lower_bound <= higher_range) {       // checking the boundaries
         guess = (lower_bound + higher_range) / 2;
         attempts++;
-        printf("I guess %d.\n", guess);
-        printf("If the answer is HIGHER than my guess, enter 'H'.\n");
-        printf("If the answer is LOWER than my guess, enter 'L'.\n");
+        printf("I guessed %d.\n", guess);
+        printf("rules of the game. \n");
+        printf("If the number you though about is HIGHER than my guess, enter 'H'.\n");
+        printf("If the number you though about is LOWER than my guess, enter 'L'.\n");
         printf("If my guess is correct, enter '='.\n");
         printf("Response: ");
         scanf(" %c", &answer);
@@ -28,15 +29,10 @@ int main() {
             higher_range = guess - 1;
         } else if (answer == '=') {
             printf("Got the right number with %d guesses!\n", attempts);
-            break;  // End the game if the correct number is guessed
+            break;  // used break to end the game if the guesses match
         } else {
-            printf("Invalid entry '%c'. Please enter H, L, or =\n", answer);
+            printf("incorrect entry '%c'. Please enter between H, L, or =\n", answer); // incase you write something that is not H,L or =
         }
-    }
-
-    // If low becomes greater than high, it means there was an inconsistency
-    if (lower_bound > higher_range) {
-        printf("Inconsistent responses detected. Exiting...\n");
     }
 
     return 0;
